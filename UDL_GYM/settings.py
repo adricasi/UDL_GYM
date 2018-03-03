1623b69f3ebdd8dca34641f4413ae9274d68636d
adricasi <adriator6@gmail.com>
2018-03-03 18:45:49 +0100

    Preparation to start work

--- UDL_GYM/settings.py
+++ UDL_GYM/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 """
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-import os
+import os.path
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -29,6 +29,9 @@ ALLOWED_HOSTS = []
 
 
 # Application definition
+TEMPLATE_DIRS = {
+    os.path.join(os.path.dirname(__file__), '../templates'),
+}
 
 INSTALLED_APPS = (
     'django.contrib.admin',
@@ -80,6 +83,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'USER': 'dbuser', 'PASSWORD': 'dbpassword',
     }
 }
 
