4b0cb2369b86ce4c60cbcb9bdbe12089d2a34380
Arnau Tallaví <atallavi@gmail.com>
2018-03-07 19:35:52 +0100

    Added empty activity_detail.html and added new view

--- udl_gym/urls.py
+++ udl_gym/urls.py
@@ -1,19 +1,25 @@
 from django.conf.urls import url, patterns
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from django.utils import timezone
 from models import Activity
 from . import views
 
+
 urlpatterns = patterns(
     '',
-    #Home
+    # Home
     url(r'^$', views.Home, name='udl_gym'),
 
-    #List activities
+    # List activities
     url(r'^Activities/$', ListView.as_view(
         queryset=Activity.objects.filter(date__lte=timezone.now()).order_by('date'),context_object_name='Activity',template_name = 'templates/activity.html'),
     name='Activities'),
 
-    #Activity detail
+    # Activity detail
+    url(r'^activity/(?P<pk>\d+)/$',
+    DetailView.as_view(
+        model = Activity,
+        template_name = 'templates/activity_detail.html',
+    ),
+    name='Activity_detail'),
 )
-
